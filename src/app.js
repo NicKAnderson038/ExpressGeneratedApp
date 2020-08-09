b70fa5aa796c008e7ae9e68ab9d4286fcac2edda
NicKAnderson038 <andersonnick038@gmail.com>
2020-08-09 00:09:27 -0400

    🎨

--- src/app.js
+++ src/app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const chalk = require('chalk')
+const log = console.log
 
-require('dotenv').config();
+require('dotenv').config()
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require('./middlewares')
+const api = require('./api')
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(helmet());
+app.use(morgan('dev'))
+app.use(helmet())
 
 app.get('/', (req, res) => {
+  log(chalk.blue('Hello') + ' World' + chalk.red('!'))
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
-});
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  })
+})
 
-app.use('/api/v1', api);
+app.use('/api/v1', api)
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-module.exports = app;
+module.exports = app
