79be7eb3fc30cb69e99ce254d437eec1acc2ed49
NicKAnderson038 <andersonnick038@gmail.com>
2020-08-22 20:32:50 -0400

    🚀

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,11 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 const db = {}
+// const db = new Map()
 
 router.post('/', ({ body }, res) => {
   for (const key in body) {
     db[key] = body[key]
+    // db.set(key, body[key])
   }
+  // console.log(...db)
+  // res.json([...db])
   res.json(db)
 })
 
